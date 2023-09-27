7222291f2a747d8c24baebb694115f70647843bb
Matt Nadareski <mnadareski@outlook.com>
2023-09-26 21:49:40 -0400

    Add .NET 8 as a framework target

--- publish-nix.sh
+++ publish-nix.sh
@@ -17,16 +17,25 @@ echo "Restoring Nuget packages"
 dotnet restore
 
 # .NET 6.0
+echo "Building .NET 6.0 releases"
 dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net6.0 -r win-x64 -c Debug --self-contained true -p:PublishSingleFile=true
 dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net6.0 -r win-x64 -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
 dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net6.0 -r linux-x64 -c Debug --self-contained true -p:PublishSingleFile=true
 dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net6.0 -r linux-x64 -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
 
 # .NET 7.0
+echo "Building .NET 7.0 releases"
 dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net7.0 -r win-x64 -c Debug --self-contained true -p:PublishSingleFile=true
 dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net7.0 -r win-x64 -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
 dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net7.0 -r linux-x64 -c Debug --self-contained true -p:PublishSingleFile=true
 dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net7.0 -r linux-x64 -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
+
+# .NET 8.0
+echo "Building .NET 8.0 releases"
+dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net8.0 -r win-x64 -c Debug --self-contained true -p:PublishSingleFile=true
+dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net8.0 -r win-x64 -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
+dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net8.0 -r linux-x64 -c Debug --self-contained true -p:PublishSingleFile=true
+dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net8.0 -r linux-x64 -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
 
 # Create archives
 cd $BUILD_FOLDER/RedBookPlayer.GUI/bin/Debug/net6.0/win-x64/publish/
@@ -46,3 +55,12 @@ cd $BUILD_FOLDER/RedBookPlayer.GUI/bin/Debug/net7.0/linux-x64/publish/
 zip -r $BUILD_FOLDER/RedBookPlayer-dbg_net7.0_linux-x64.zip .
 cd $BUILD_FOLDER/RedBookPlayer.GUI/bin/Release/net7.0/linux-x64/publish/
 zip -r $BUILD_FOLDER/RedBookPlayer_net7.0_linux-x64.zip .
+
+cd $BUILD_FOLDER/RedBookPlayer.GUI/bin/Debug/net8.0/win-x64/publish/
+zip -r $BUILD_FOLDER/RedBookPlayer-dbg_net8.0_win-x64.zip .
+cd $BUILD_FOLDER/RedBookPlayer.GUI/bin/Release/net8.0/win-x64/publish/
+zip -r $BUILD_FOLDER/RedBookPlayer_net8.0_win-x64.zip .
+cd $BUILD_FOLDER/RedBookPlayer.GUI/bin/Debug/net8.0/linux-x64/publish/
+zip -r $BUILD_FOLDER/RedBookPlayer-dbg_net8.0_linux-x64.zip .
+cd $BUILD_FOLDER/RedBookPlayer.GUI/bin/Release/net8.0/linux-x64/publish/
+zip -r $BUILD_FOLDER/RedBookPlayer_net8.0_linux-x64.zip .
--- publish-win.bat
+++ publish-win.bat
@@ -30,6 +30,13 @@ dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net7.0 -r win-x64 -
 dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net7.0 -r linux-x64 -c Debug --self-contained true -p:PublishSingleFile=true
 dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net7.0 -r linux-x64 -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
 
+REM .NET 8.0
+echo Building .NET 8.0 releases
+dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net8.0 -r win-x64 -c Debug --self-contained true -p:PublishSingleFile=true
+dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net8.0 -r win-x64 -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
+dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net8.0 -r linux-x64 -c Debug --self-contained true -p:PublishSingleFile=true
+dotnet publish RedBookPlayer.GUI/RedBookPlayer.GUI.csproj -f net8.0 -r linux-x64 -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
+
 REM Create archives
 cd %BUILD_FOLDER%\RedBookPlayer.GUI\bin\Debug\net6.0\win-x64\publish\
 7z a -tzip %BUILD_FOLDER%\RedBookPlayer-dbg_net6.0_win-x64.zip *
@@ -48,3 +55,12 @@ cd %BUILD_FOLDER%\RedBookPlayer.GUI\bin\Debug\net7.0\linux-x64\publish\
 7z a -tzip %BUILD_FOLDER%\RedBookPlayer-dbg_net7.0_linux-x64.zip *
 cd %BUILD_FOLDER%\RedBookPlayer.GUI\bin\Release\net7.0\linux-x64\publish\
 7z a -tzip %BUILD_FOLDER%\RedBookPlayer_net7.0_linux-x64.zip *
+
+cd %BUILD_FOLDER%\RedBookPlayer.GUI\bin\Debug\net8.0\win-x64\publish\
+7z a -tzip %BUILD_FOLDER%\RedBookPlayer-dbg_net8.0_win-x64.zip *
+cd %BUILD_FOLDER%\RedBookPlayer.GUI\bin\Release\net8.0\win-x64\publish\
+7z a -tzip %BUILD_FOLDER%\RedBookPlayer_net8.0_win-x64.zip *
+cd %BUILD_FOLDER%\RedBookPlayer.GUI\bin\Debug\net8.0\linux-x64\publish\
+7z a -tzip %BUILD_FOLDER%\RedBookPlayer-dbg_net8.0_linux-x64.zip *
+cd %BUILD_FOLDER%\RedBookPlayer.GUI\bin\Release\net8.0\linux-x64\publish\
+7z a -tzip %BUILD_FOLDER%\RedBookPlayer_net8.0_linux-x64.zip *
